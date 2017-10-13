b741979184aa07bc2281b9404a25f329b49ad5a8
Jacob Callahan <jacob.callahan05@gmail.com>
2017-10-13 08:43:54 -0400

    Add hypervisor and guest spoofing. The flood.py script now requires jacobcallahan/genvirt for virt-who spoofs. See readme for usage.

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # content-host-d
-A Docker-ized Centos Content Host for Red Hat Satellite 6
-Current tags are: 5, 6, 7, latest
+A Docker-ized RHEL 7 Hypervisor Guest for Red Hat Satellite 6
 
 Installation
 ------------
-```docker pull jacobcallahan/content-host-d:<tag>```
+```docker build -t ch-d:guest <path-to-this-directory>```
 
 Usage
 -----
-```docker run <arguments> jacobcallahan/content-host-d:<tag>```
+```docker run <arguments> ch-d:guest```
+
+Additonally, a fully-automated flood script is provided that will create fake hypervisors and guests.
+
+```python flood.py -s my.sat.host.com --hypervisors 5 --guests 5 --limit 20``` 
 
 Accepted Arguments
 ------------------
@@ -19,6 +22,7 @@ Accepted Arguments
  * KILL - If this is not passed, then the container will be kept alive and goferd running.
  * ORG - Name of the Organization to use.
  * SATHOST(Required) - Hostname of the Satellite (not url).
+ ^ UUID - Will set a virt uuid for subscription manager to report.
 
 Note
 ----
@@ -26,14 +30,8 @@ If you want to be able to use katello-agent, you must mount your /dev/log to the
 
 Examples
 --------
-```docker run -e "SATHOST=my.host.domain.com" jacobcallahan/content-host-d:5```
+```docker run -e "SATHOST=my.host.domain.com" -e "UUID=my-long-uuid" ch-d:guest```
 
-```docker run -e "SATHOST=my.host.domain.com" -e "ENV=Dev" -e "AUTH=username/password" jacobcallahan/content-host-d:6```
+```docker run -e "SATHOST=my.host.domain.com" -e "ORG=Dev" -e "AUTH=username/password" -e "UUID=my-long-uuid" ch-d:guest```
 
-```docker run -d -e "SATHOST=my.host.domain.com" -e "AK=sat62" -e "KILL=1" jacobcallahan/content-host-d:7```
-
-```docker run -h DockerCH -v /dev/log:/dev/log -d -e "SATHOST=my.host.domain.com" -e "AK=sat62" jacobcallahan/content-host-d:5```
-
-```for i in {1..10}; do docker run -d -e "SATHOST=my.host.domain.com" -e "AK=sat62" -e "KILL=1" jacobcallahan/content-host-d:6; done;```
-
-```for i in {1..10}; do docker run -d -h Docker$i -e "SATHOST=my.host.domain.com" -e "AK=sat62" jacobcallahan/content-host-d:7; done;```
+```python flood.py -s my.host.domain.com --hypervisors 5 --guests 5 --limit 10```
--- flood.py
+++ flood.py
@@ -1,13 +1,30 @@
-import argparse
+import argparse, json, os, time, uuid
 from collections import deque
-from time import time
 import docker
 
+def gen_json(hypervisors, guests):
+    virtwho = {}
+    all_guest_list = []
+    for i in range(hypervisors):
+        guest_list = []
+        for c in range(guests):
+            cur_id = str(uuid.uuid4())
+            guest_list.append({
+                "guestId": cur_id,
+                "state": 1,
+                "attributes": {
+                    "active": 1,
+                    "virtWhoType": "esx"
+                }
+            })
+            all_guest_list.append(cur_id)
+        virtwho[str(uuid.uuid4()).replace("-", ".")] = guest_list
+    return (virtwho, all_guest_list)
 
 def rm_container(client, containers):
-    del_container = containers.popleft()
+    del_container = containers.pop(0)
     client.remove_container(del_container['container'], v=True, force=True)
-    print ('Done with container #{0}'.format(del_container['number']))
+    print ('Done with {0}'.format(del_container['name']))
 
 def host_flood(count, host, key, tag, name, limit, image, criteria, org):
     client = docker.Client(version='1.22')  # docker.from_env()
@@ -22,7 +39,7 @@ def host_flood(count, host, key, tag, name, limit, image, criteria, org):
                 environment={'SATHOST': host, 'AK': key, 'ORG': org},
                 volumes='/dev/log:/dev/log:Z'
             )
-            containers.append({'container': container, 'number': num})
+            containers.append({'container': container, 'name': num})
             client.start(container=container)
             print ('Created: {0}'.format(num))
             num += 1
@@ -35,30 +52,84 @@ def host_flood(count, host, key, tag, name, limit, image, criteria, org):
             if 'Complete!'.encode() in logs:
                 rm_container(client, containers)
         else:
-            if time() - containers[0].get('delay', time()) >= criteria:
+            if time.time() - containers[0].get('delay', time.time()) >= criteria:
                 rm_container(client, containers)
             elif not containers[0].get('delay', False) and 'Complete!'.encode() in logs:
-                containers[0]['delay'] = time()
+                containers[0]['delay'] = time.time()
             elif client.inspect_container(containers[0]['container']['Id'])['State']['Status'] != u'running':
                 rm_container(client, containers)
+
+def virt_flood(host, tag, limit, image, org, hypervisors, guests):
+    virt_data, guest_list = gen_json(hypervisors, guests)
+    with open('/tmp/temp.json', 'w') as f:
+        json.dump(virt_data, f)
+    client = docker.Client(version='1.22')
+    print ("Submitting virt-who report. Note: this will create a host: 'meeseeks'.")
+    container = client.create_container(
+        image='jacobcallahan/genvirt',
+        hostname='meeseeks',
+        detach=False,
+        environment={'SATHOST': host},
+        volumes='/tmp/temp.json',
+        host_config=client.create_host_config(binds={
+            '/tmp/temp.json': {'bind': '/tmp/temp.json', 'mode': 'ro'}
+        })
+    )
+    client.start(container=container)
+    while 'Done!'.encode() not in client.logs(container):
+        time.sleep(2)
+    client.remove_container(container, v=True, force=True)
+    os.remove('/tmp/temp.json')
+    _ = input("Pausing for you to attach subscriptions to the new hypervisors.")
+
+    print("Starting guest creation.")
+    active_hosts = []
+    while guest_list or active_hosts:
+        if guest_list and len(active_hosts) < limit:
+            guest = guest_list.pop(0)
+            name = 'flood{}'.format(guest.split('-')[4])
+            container = client.create_container(
+                image='{0}:{1}'.format(image, tag),
+                hostname=name,
+                detach=False,
+                environment={'SATHOST': host, 'ORG': org, 'UUID': guest}
+            )
+            active_hosts.append({'container': container, 'name': name})
+            client.start(container=container)
+            print ('Created Guest: {}. {} left in queue.'.format(name, len(guest_list)))
+
+        logs = client.logs(active_hosts[0]['container']['Id'])
+        # We'll wait for 30 seconds after attempting to auto-attach
+        if time.time() - active_hosts[0].get('delay', time.time()) >= 30:
+            rm_container(client, active_hosts)
+        elif not active_hosts[0].get('delay', False) and 'auto-attach'.encode() in logs:
+            active_hosts[0]['delay'] = time.time()
+        elif client.inspect_container(active_hosts[0]['container']['Id'])['State']['Status'] != u'running':
+            rm_container(client, active_hosts)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "tag", type=str,
+        "-t", "--tag", type=str,
         help="The image tag you want the container based on. ch-d:<tag>")
     parser.add_argument(
-        "host", type=str,
+        "-s", "--satellite", type=str,
         help="The hostname of the target Satellite.")
     parser.add_argument(
-        "name", type=str,
+        "-n", "--name", type=str,
         help="The base hostname to use for the containers.")
     parser.add_argument(
-        "key", type=str,
+        "-k", "--key", type=str,
         help="The Activation Key to use for registration.")
     parser.add_argument(
-        "count", type=int,
+        "-c", "--count", type=int,
         help="The number of docker content hosts to create.")
+    parser.add_argument(
+        "--hypervisors", type=int,
+        help="The number of hypervisors to create.")
+    parser.add_argument(
+        "--guests", type=int,
+        help="The number of guests per hypervisor to create.")
     parser.add_argument(
         "--org", type=int, help="The organization to register hosts to "
         "(defaults to 'Default_Organization'.")
@@ -89,9 +160,16 @@ if __name__ == '__main__':
     else:
         criteria = 60
 
-    print ("Starting content host creationi with criteria {}.".format(criteria))
-    host_flood(
-        args.count, args.host, args.key, args.tag,
-        args.name, limit, image, criteria, org
-    )
+    if args.hypervisors:
+        print ("Starting population of hypervisor(s) and guest(s)")
+        host = "$(hostname)" if not args.satellite else args.satellite
+        tag = 'guest' if not args.tag else args.tag
+        guests = 'guest' if not args.guests else args.guests
+        virt_flood(host, tag, limit, image, org, args.hypervisors, guests)
+    else:
+        print ("Starting content host creation with criteria {}.".format(criteria))
+        host_flood(
+            args.count, args.host, args.key, args.tag,
+            args.name, limit, image, criteria, org
+        )
     print ("Finished content host creation.")
